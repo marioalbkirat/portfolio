bab64af16bfee5994e2ec14d9b507c8dc569e12d
marioalbkirat <mario.albkirat@gmail.com>
2025-10-26 17:04:22 +0300

    update google

--- src/App.js
+++ src/App.js
@@ -4,7 +4,19 @@ import './MQ.css';
 import Home from './Pages/Home/Home';
 import NotFound from './Pages/NotFound/NotFound';
 import Header from './components/Navber/Navber';
+import { useEffect } from 'react';
 function App() {
+    useEffect(() => {
+        // تأكد أن gtag موجود
+        if (window.gtag) {
+            window.gtag("event", "page_view", {
+                page_path: window.location.pathname,
+            });
+            console.log("✅ Sent manual page_view event");
+        } else {
+            console.warn("⚠️ gtag not found yet");
+        }
+    }, []);
     return (
         <BrowserRouter>
             <Header />
